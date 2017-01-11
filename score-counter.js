86567efae7ce844edfc31004dfb29199e0041167
Szymon Maruda <szymon.maruda@gmail.com>
2017-01-11 22:30:42 +0100

    2nd version

--- score-counter.js
+++ score-counter.js
@@ -1,50 +1,60 @@
 var ScoreCounter = function(place, size, current) {
-  this.place=place;
-  this.size=size;
-  this.current=current;
+    this.place = place;
+    this.size = size;
+    this.current = current;
 
-  this.up=function(){
-    var tmp = this.current+1;
-    var sNumber = tmp.toString();
+    this.up = function() {
+        if(typeof(this.current)=="string"){
+          this.current=Number(this.current);
+        }
+        var tmp = this.current + 1;
 
-    if (sNumber.length <= this.size){
-      ++this.current;
-      return pad(this.current, this.size);
-    }else{
-      return pad(this.current, this.size);
+        var sNumber = tmp.toString();
+
+        if (sNumber.length <= this.size) {
+
+            ++this.current;
+            return this.current;
+        } else {
+            return this.current;
+        }
     }
-  }
 
-  this.down=function(){
-    --this.current;
-    if(this.current <= 0){
-      this.current = 0;
+    this.down = function() {
+        --this.current;
+        if (this.current <= 0) {
+            this.current = 0;
+        }
+        return this.current;
     }
-    return pad(this.current, this.size);
-  }
 
 
-  this.valuee=function(val){
-    if(val == undefined){return this.current;}else{
-      var old_current=this.current;
-      this.current=val;
-      if(this.size >= this.current.length){
-        return pad(this.current, this.size);
-      }else{
-        this.current=old_current;
-        return pad(this.current, this.size);
-      }
+    this.valuee = function(val) {
+
+        if (val == undefined) {
+            return this.current;
+        } else {
+
+            var old_current = this.current;
+            this.current = val;
+            if (this.size >= this.current.length) {
+                return this.current;
+            } else {
+
+                this.current = old_current;
+                return this.current;
+            }
+        }
     }
-  }
 
-  this.reset=function(){
-    this.current=0;
-    return pad(this.current, this.size);
-  }
+    this.reset = function() {
+        this.current = 0;
+        return this.current;
+    }
+
+    this.pad = function(n, width) {
+        z = '0';
+        n = n + '';
+        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    }
 };
-
-function pad(n, width) {
-  z = '0';
-  n = n + '';
-  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
--- score-ui.js
+++ score-ui.js
@@ -1,26 +1,25 @@
-var dummy = function() {
-
-}
-
 var ScoreUI = function(place, size, current) {
     this.place = place;
+
     var new_place = this.place.getAttribute("id");
 
     this.size = size;
     this.current = current;
     var scoreCounter = new ScoreCounter(document.querySelector('.counter'), this.size, this.current);
+    var cntr_place = scoreCounter.place.getAttribute("class");
 
-    document.getElementById(new_place).querySelector(".value").value = pad(this.current, this.size);
+    document.getElementById(new_place).querySelector("."+cntr_place).innerHTML = scoreCounter.pad(this.current, this.size);
 
 
     document.getElementById(new_place).querySelector(".up").onclick = function() {
         this.current = scoreCounter.up();
-        document.getElementById(new_place).querySelector(".value").value = this.current;
+
+        document.getElementById(new_place).querySelector(".counter").innerHTML = scoreCounter.pad(this.current, size);
     };
 
     document.getElementById(new_place).querySelector(".down").onclick = function() {
         this.current = scoreCounter.down();
-        document.getElementById(new_place).querySelector(".value").value = this.current;
+        document.getElementById(new_place).querySelector(".counter").innerHTML = scoreCounter.pad(this.current, size);
     };
 
     document.getElementById(new_place).querySelector(".get").onclick = function() {
@@ -31,29 +30,33 @@ var ScoreUI = function(place, size, current) {
     document.getElementById(new_place).querySelector(".set").onclick = function() {
         var val = document.getElementById(new_place).querySelector(".value").value;
         this.current = scoreCounter.valuee(val);
-        document.getElementById(new_place).querySelector(".value").value = this.current;
+        document.getElementById(new_place).querySelector(".counter").innerHTML = scoreCounter.pad(this.current, size);
     };
 
     document.getElementById(new_place).querySelector(".reset").onclick = function() {
         this.current = scoreCounter.reset();
-        document.getElementById(new_place).querySelector(".value").value = this.current;
+        document.getElementById(new_place).querySelector(".counter").innerHTML = scoreCounter.pad(this.current, size);
     };
-}
-document.querySelector(".add_counter").onclick = function() {
-    var number = document.getElementsByClassName("counter").length + 1;
-    var div = document.createElement('div');
-    document.getElementById('page-wrapper').appendChild(div);
-    div.id = "score-" + number;
-    div.innerHTML = document.getElementById("page-wrapper").firstElementChild.innerHTML;
-    div.getElementsByTagName("h1")[0].firstChild.nodeValue = "Score " + number + ":";
 
-    new ScoreUI(document.getElementById(div.id), 4, 0);
+    $('form').submit(function(e){
+        e.preventDefault();
+    });
 
-}
+    document.querySelector(".add_counter").onclick = function() {
+        var number = document.getElementsByClassName("counter").length + 1;
+        var div = document.createElement('div');
+        document.getElementById('page-wrapper').appendChild(div);
+        div.id = "score-" + number;
+        div.innerHTML = document.getElementById("page-wrapper").firstElementChild.innerHTML;
+        div.getElementsByTagName("h1")[0].firstChild.nodeValue = "Score " + number + ":";
 
-document.querySelector(".delete_counter").onclick = function() {
-    var counters = document.getElementById('page-wrapper').getElementsByTagName('div');
-    if (counters.length > 3) {
-        counters[counters.length - 1].remove();
+        new ScoreUI(document.getElementById(div.id), 4, 0);
+    }
+
+    document.querySelector(".delete_counter").onclick = function() {
+        var counters = document.getElementById('page-wrapper').getElementsByTagName('div');
+        if (counters.length > 3) {
+            counters[counters.length - 1].remove();
+        }
     }
 }
